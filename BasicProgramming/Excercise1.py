da1a0ca91c35127d07477dacad9f77fc438b4d76
Sandeep Kumar <sandeep.kumar_84@yahoo.co.in>
2024-09-26 00:17:17 +0100

    formatting and students metadata

--- BasicProgramming/Excercise1.py
+++ BasicProgramming/Excercise1.py
@@ -1,4 +1,9 @@
+#Module Code: B9DA108 | Module Title: Programming for Data Analysis | School: DBS 
+#Lecturer Name: Alexander Victor
 #Exercise 1: Week2: Python Basics To be completed by date:Tuesday, 1 October 2024, 11:00 AM
+#Student Name: Sandeep Kumar | Student ID: 20049275 | Email: 20049275@mydbs.ie | Git: https://github.com/sandeepkumar-84/DBS.git
+
+#Importing time, random and match libraries to be used in the code. 
 
 import time
 import random
@@ -52,9 +57,6 @@ print(f"   4 is {check_odd_or_even(4)} number\n")
 
 #6. Generate a random number between 1 and 100 and ask the user to guess it.
 print("6. Generate a random number between 1 and 100 and ask the user to guess it.")
-
-
-
 sys_random_num = random.randrange(1,100)
 user_rand_num = input("   System has generated a random number. Can you guess it?")
 
@@ -63,11 +65,13 @@ if(sys_random_num == user_rand_num):
 else:
     print(f"   Your guess {user_rand_num} is incorrect as system generated random number was {sys_random_num}\n")
 
+# Sleep/delay is added so that user can check the result and then other code is executed
 time.sleep(3)
 #7. Write a function to check if a given string is a palindrome.
 print("7. Write a function to check if a given string is a palindrome.")
 
-
+# Characters are compared from start and end of the list of the input string. loop executes till the middle of it. 
+#is_palindrome is true until characters are found different breaking the loop. 
 def check_palindrome(word):
         length_word = len(word)
         start = 0
@@ -99,6 +103,8 @@ diplay_palidrome_results("Noon")
 
 #8. Calculate the factorial of a given number.
 print("8. Calculate the factorial of a given number.")
+
+# Recursive approach is used till number is reduced to 1 or 0. 
 
 def find_factorial(num):
     if(num ==0 or num == 1):
@@ -147,14 +153,19 @@ second_number = int(input("Please provide second number = "));
 operation = input("Please provide operation by either selecting serial number or operation from 1. Addition 2. Substraction 3. Multiplication 4. Division = ");
 
 print(f"Result of the above calculation is {calculator_simulator(first_number, second_number, operation)}\n")
-
+# Sleep/delay is added so that user can check the result and then other code is executed
 time.sleep(3)
+
 #11. Write a function to find the square root of a number using Newton's method.
 print("11. Write a function to find the square root of a number using Newton's method.")
+
+# Newtons formula for iterations is defined in a function
 def sql_root_iteration(xn,num):
     xnp1 = (xn + (num/xn))/2
     return xnp1
 
+# Newtons iteration function is called using this function. Guess is assumed to be input number by 2.
+# Range  is hard coded to 16 for large values 
 def cal_newton_root(num):
   listIterations = []
   guess = num/2
@@ -186,6 +197,9 @@ print(f"   The reverse of the list {list_str} is {rev_givent_str(list_str)}\n")
 #13. Write a program to find all the prime numbers between 1 and 100.
 print("13. Write a program to find all the prime numbers between 1 and 100.")
 
+# Loop starts from 4 as 2 and 3 are Natural primes, untill it reaches the 100 
+# Inner loop checks the divisibilty from 2 till the number itself. If anytime a divisor is found loop exits 
+# else if no divisor is found then prime number is appended to the list.    
 def find_prime_lt(num):
     checkNum =  4
     list_prime = []
@@ -210,6 +224,7 @@ print(f"   List of prime numbers less than 100 is {find_prime_lt(100)}\n")
 #14. Calculate the Fibonacci sequence up to a given number.
 print(f"14. Calculate the Fibonacci sequence up to a given number.")
 
+# recursive approach is used in function to find the nth fibonacci. 
 def find_fibonacci(num):
     if num == 1:
         
@@ -221,7 +236,7 @@ def find_fibonacci(num):
         
         return find_fibonacci(num - 1) + find_fibonacci(num - 2)
 
-
+# function prints all the n fibonacci numbers until it is less than given input (sum ) 
 def find_fibonacdi_lt(sum):
     less_than = 1
     while find_fibonacci(less_than) < sum:
@@ -236,6 +251,7 @@ print("\n")
 #15. Write a function to count the number of vowels in a given string.
 print("15. Write a function to count the number of vowels in a given string.")
 
+# vowels are predefined in a list and input is looped through and each alphabet is checked against the predefined list. 
 def count_vowels(given_str):
     count = 0
     list_vowels = ['a','e','i','o','u']
@@ -263,6 +279,8 @@ print(f"   Year 2025 is  a {check_leap_year(2025)}\n")
 #17. Remove duplicates from a list without using sets.
 print("17. Remove duplicates from a list without using sets.")
 
+# unique list is constructed in a loop by iterating through each character in the original string. 
+# if the charatcter is already added, it will not be appended. 
 def remove_duplicates(listOrig):
     listUniq = []
     for x in listOrig:
@@ -295,11 +313,19 @@ print(f"    List after bubble sort  {bubble_sort(listOrig)}\n")
 print("19. Create a dictionary of names and ages, and print the name of the oldest person.")
 old_age = {"John":"58","Dow":"67","Paul":"60","Zen":"65"}
 print(f"    The old age dictionary looks like {old_age}")
+
+#items of the dictionary is queried for max age value
 oldest_person = [name for name, age in old_age.items() if age == max(old_age.values())]
 print(f"    The oldest person in the above dictionary is {str(oldest_person)[2:-21]}")
 
 #20. Write a program to find the least common multiple (LCM) of two numbers.
 print("20. Write a program to find the least common multiple (LCM) of two numbers.")
+
+#basic approach of  is used
+# lists for first input numbers is created, this will hold the multipliers of the num1 like num1*1, num1*2....
+# lists for first input numbers is created, this will hold the multipliers of the num2 like num2*1, num2*2.... 
+# inner for loop, loops through first list and check each item in list 2. If item is present then it is the lcm and loop breaks 
+# outer while loop runs until lcm is found.  
 
 def find_lcm(num1, num2):
     factorsNum1 = []
