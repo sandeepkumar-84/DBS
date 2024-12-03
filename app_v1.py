f98c915436682fc0d7dca02b2e4541d5122d13b7
sandeepkumar-84 <36576549+sandeepkumar-84@users.noreply.github.com>
2024-12-03 18:23:50 +0000

    Update app_v1.py
    Added comments

--- app_v1.py
+++ app_v1.py
@@ -1,16 +1,62 @@
+'''
+    Application File - To generate UI to intercat with users. It is integrated with the Bert model.
+
+    Creating the UI for the DBS chatbot using the Tkinter (Tk interface). 
+    It is a standard Python GUI (Graphical User Interface) library that provides a set of tools and widgets 
+    to create desktop applications with graphical interfaces. 
+    
+    
+    Created by: Sandeep Kumar
+    Student ID: 20049275
+    University: DBS
+    Module Title: Programming for Data Analysis Project
+    Lecturer: Alexander Victor
+
+'''
 from tkinter import *
 from Research_chatbot_6 import provide_res_to_ui
 
-BG_GRAY = "#ABB2B9"
-BG_COLOR = "#17202A"
-TEXT_COLOR = "#EAECEE"
 
-FONT = "Helvetica 14"
-FONT_BOLD = "Helvetica 13 bold"
+# Declaring the font color and font of the text
+FONT_COL = "#4E342E"
+FONT = "Poppins 14"
+FONT_BOLD = "Poppins 13 bold"
 
-BOT_NAME = "DBS Bot"
+# Declaring the Footer background color and the main chat window color giving it a warm look
+FOOTER_BG_COL = "#ABB2B9"
+BODY_BG_COL = "#F7F1E3"
 
-class ChatApplication:
+# Calling the bot as DBS Bot
+DBS_BOT_NAME = "DBS Bot"
+
+'''
+ Defining a class DBS application. 
+ It has a constructor which is called whenever a new instance of the class is instantiated. 
+ Constructor creates main application window using TK. In this window widgets or other components can be added.
+ Thus it will act as a container for other objects. 
+ 
+ function run(self) executes the main functionality of the class. In this case it is main event loopof GUI app. 
+ It listens for user actions, events like clicks, text inputs etc. 
+
+    _setup_main_window function is called from constructor. It performs following execution
+    setting up of the title. 
+    configuring the parameters like making it non resizable so that user cannot change its size.
+    configuring its height and width. 
+    creation of label widget for welocome message for DBS chatbot. 
+    creation of a horizontal line with the help of label widget by limiting its width to 1
+    creation of a text widget, configuring its parameters like color and font, width and height. 
+    Width paremeter represents the lenght of the sentence before it can be wrapped. 
+    cursor="arrow" means how the cursor will look on the chat main window.
+    creation of a scrollbar widget. It is placed in the text widget. 
+    footer label for grey area of the chat window.
+    creation of text entry field. entry widget is binded 
+    with _on_enter_pressed event so that if user provides any input text and enter is pressed
+    _on_enter_pressed will fire and exetutes its code. 
+    creation of send button widget. 
+    Its command parameter can be used to specify function (here it is _on_enter_pressed) to be attached to when it is clicked. 
+
+'''
+class DBSChatApplication:    
     def __init__(self):
         self.window = Tk()
         self._setup_main_window()
@@ -21,57 +67,68 @@ class ChatApplication:
     def _setup_main_window(self):
         self.window.title("DBS Chat")
         self.window.resizable(width=False, height=False)
-        self.window.configure(width=470, height=550, bg=BG_COLOR)
+        self.window.configure(width=450, height=500, bg=BODY_BG_COL)
 
-        head_label = Label(self.window, bg=BG_COLOR, fg = TEXT_COLOR, text="Welcome", font=FONT_BOLD, pady=10)
-        head_label.place(relwidth=1)
+        label_welcome = Label(self.window, bg=BODY_BG_COL, fg = FONT_COL, text="Hi there! Welcome to DBS’s virtual assistant.", font=FONT_BOLD, pady=10)
+        label_welcome.place(relwidth=1)
 
-        line=Label(self.window, width=450, bg = BG_GRAY)
-        line.place(width=1, rely=0.07, relheight=0.012)
+        label_line = Label(self.window, width=430, bg = FOOTER_BG_COL)
+        label_line.place(width=2, rely=0.08, relheight=0.014)
 
-        self.text_widget = Text(self.window, width=20, height=2, bg = BG_COLOR, fg=TEXT_COLOR, font=FONT, padx=5, pady=5)
-        self.text_widget.place(relheight=0.745, relwidth=1, rely=0.08)
-        self.text_widget.configure(cursor="arrow", state=DISABLED)
+        self.text_dbs_widget = Text(self.window, width=18, height=2, bg = BODY_BG_COL, fg=FONT_COL, font=FONT, padx=5, pady=5)
+        self.text_dbs_widget.place(relheight=0.746, relwidth=1, rely=0.07)
+        self.text_dbs_widget.configure(cursor="arrow", state=DISABLED)
 
-        scrollbar =Scrollbar(self.text_widget)
+        scrollbar =Scrollbar(self.text_dbs_widget)
         scrollbar.place(relheight=1, relx=0.974)
-        scrollbar.configure(command=self.text_widget.yview)
+        scrollbar.configure(command=self.text_dbs_widget.yview)
 
-        bottom_label = Label(self.window, bg=BG_GRAY, height=80)
-        bottom_label.place(relwidth=1,rely=0.825)
+        label_footer = Label(self.window, bg=FOOTER_BG_COL, height=80)
+        label_footer.place(relwidth=1,rely=0.825)
 
-        self.msg_entry = Entry(bottom_label, bg="#2C3E50", fg=TEXT_COLOR, font=FONT)
-        self.msg_entry.place(relwidth=0.74, relheight=0.06, rely=0.008, relx=0.011)
-        self.msg_entry.focus()
-        self.msg_entry.bind("<Return>",self._on_enter_pressed)
+        self.entry_text = Entry(label_footer, bg=BODY_BG_COL, fg=FONT_COL, font=FONT)
+        self.entry_text.place(relwidth=0.72, relheight=0.05, rely=0.007, relx=0.010)
+        self.entry_text.focus()
+        self.entry_text.bind("<Return>",self._on_enter_pressed)
 
-        send_button = Button(bottom_label, text="Send", font=FONT_BOLD, width=20, bg=BG_GRAY, 
+        button_send = Button(label_footer, text="Send", font=FONT_BOLD, width=20, bg=FOOTER_BG_COL, 
                              command=lambda:self._on_enter_pressed(None))
-        send_button.place(relx=0.77, rely=0.008, relheight=0.06, relwidth=0.22)
+        button_send.place(relx=0.76, rely=0.007, relheight=0.05, relwidth=0.22)
 
 
-
+    '''
+        method _on_enter_pressed is defined to get the input text (from entry_text) 
+        and passes on to another method _insert_message
+    '''
     def _on_enter_pressed(self, event):
-        msg = self.msg_entry.get()
+        msg = self.entry_text.get()
         self._insert_message(msg,"You")
-
+    '''
+        method _insert_message takes the message as an input parameter and insert it into the text_dbs_widget
+        for that it has to first enable it and then disable it back. 
+        
+        it also calls the provide_res_to_ui function of the Research_chatbot_6.py file to fetch the response from the 
+        pretrained model. The returned message is appended with bot name and inserted into the text_dbs_widget. 
+        see(END) function makes sure that we are always at the end of conversation. 
+    '''
     def _insert_message(self, msg, sender):
         if not msg:
             return
 
-        self.msg_entry.delete(0,END)
+        self.entry_text.delete(0,END)
         msg1 = f"{sender}:{msg}\n\n"
-        self.text_widget.configure(state=NORMAL)
-        self.text_widget.insert(END,msg1)
-        self.text_widget.configure(state=DISABLED)
+        self.text_dbs_widget.configure(state=NORMAL)
+        self.text_dbs_widget.insert(END,msg1)
+        self.text_dbs_widget.configure(state=DISABLED)
 
-        msg2 = f"{BOT_NAME}:{provide_res_to_ui(msg)}\n\n"
-        self.text_widget.configure(state=NORMAL)
-        self.text_widget.insert(END,msg2)
-        self.text_widget.configure(state=DISABLED)        
+        msg2 = f"{DBS_BOT_NAME}:{provide_res_to_ui(msg)}\n\n"
+        self.text_dbs_widget.configure(state=NORMAL)
+        self.text_dbs_widget.insert(END,msg2)
+        self.text_dbs_widget.configure(state=DISABLED)        
 
-        self.text_widget.see(END)
+        self.text_dbs_widget.see(END)
 
+#app execution code. 
 if __name__ == "__main__":
-    app = ChatApplication()
+    app = DBSChatApplication()
     app.run()
